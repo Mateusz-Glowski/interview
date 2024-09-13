069de508a288195c22c6bc6e43911d19586802af
Mateusz-Glowski <mateusz.glowski.96@gmail.com>
2024-09-13 15:52:50 +0200

    add /aggregateMarketData endpoint

--- src/binance/binance-client.service.ts
+++ src/binance/binance-client.service.ts
@@ -1,20 +1,42 @@
-import { Injectable } from "@nestjs/common";
-import { HttpService } from "@nestjs/axios";
-import config from "../config";
-import { KlinesResponse } from "./interfaces/klinesResponse";
-import { map } from "rxjs";
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import config from '../config';
+import {
+  KlinesResponse,
+  KlinesResponseProperties,
+} from './interfaces/klinesResponse';
+import { map, Observable } from 'rxjs';
+import { chunk } from 'lodash';
 
 @Injectable()
 export class BinanceClientService {
-  constructor(private readonly httpService: HttpService) {
-  }
+  constructor(private readonly httpService: HttpService) {}
 
   /**
    * Fetch historical market data for a specific cryptocurrency symbol and time range.
    */
-  fetchMarketData(symbol: string) {
+  fetchMarketData(symbol: string, startTime: number, endTime: number) {
     // For simplicity purposes for now I'm assuming constant inverval = 1 minute.
-    return this.httpService.get<KlinesResponse>(`${config.binanceApiUrl}/klines?symbol=${symbol}&interval=1m`)
-      .pipe(map(res => res.data));
+    return this.httpService
+      .get<KlinesResponse>(
+        `${config.binanceApiUrl}/klines?symbol=${symbol}&interval=1m&startTime=${startTime}&endTime=${endTime}`,
+      )
+      .pipe(map((res) => res.data));
+  }
+
+  aggregateMarketData(
+    symbol: string,
+    startTime: number,
+    endTime: number,
+    chunkSize: number = 10,
+  ): Observable<number[][]> {
+    return this.fetchMarketData(symbol, startTime, endTime).pipe(
+      map((data) => {
+        const closePrice = data.map((x) =>
+          Number(x[KlinesResponseProperties.closePrice]),
+        );
+        return chunk(closePrice, chunkSize);
+      }),
+    );
   }
 }
--- src/binance/binance/binance.controller.ts
+++ src/binance/binance/binance.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get, Query } from "@nestjs/common";
-import { BinanceClientService } from "../binance-client.service";
+import { Controller, Get, Query } from '@nestjs/common';
+import { BinanceClientService } from '../binance-client.service';
 
 @Controller('binance')
 export class BinanceController {
-  constructor(private readonly binanceService: BinanceClientService) {
-  }
+  constructor(private readonly binanceService: BinanceClientService) {}
 
   @Get('fetchMarketData')
-  fetchMarketData(@Query('symbol') symbol: string) {
-    return this.binanceService.fetchMarketData(symbol)
+  fetchMarketData(
+    @Query('symbol') symbol: string,
+    @Query('startTime') startTime: number, // In millis.
+    @Query('endTime') endTime: number,
+  ) {
+    return this.binanceService.fetchMarketData(symbol, startTime, endTime);
+  }
+
+  @Get('aggregateMarketData')
+  aggregateMarketData(
+    @Query('symbol') symbol: string,
+    @Query('startTime') startTime: number,
+    @Query('endTime') endTime: number,
+  ) {
+    return this.binanceService.aggregateMarketData(symbol, startTime, endTime);
   }
 }
